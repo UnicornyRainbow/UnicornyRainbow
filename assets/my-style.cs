518bd7f8017c3166cf4a710ce5840e6d61bbe4e1
Unicorn <unicornyrainbow@posteo.de>
2022-08-14 20:53:50 +0200

    Create my-style.cs

---
+++ assets/my-style.cs
@@ -0,0 +1,59 @@
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  margin-top: 1%;
+  margin-left: 1%;
+  margin-right: 1%;
+}
+
+body div {
+  min-height: 100%;
+  background-color: #ebd5c7;
+}
+
+img {
+   max-width: 100%;
+}
+
+hr {
+  border-bottom: 1px solid #000;
+}
+
+nav {
+  position: sticky;
+  top: 0;
+}
+
+nav ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #342f2d;
+}
+
+nav li {
+  float: left;
+}
+
+nav li a {
+  display: block;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+}
+
+.navbar-link {
+  padding: 14px 16px;
+}
+
+nav li a:hover {
+  background-color: #111;
+}
+.active {
+  background-color: #ebd5c7;
+  color: black;
+}
