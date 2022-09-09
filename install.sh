39917b6952f76b283af5f4719e7ee0718b33ae65
UnicornyRainbow <unicornyrainbow@posteo.de>
2022-09-09 15:59:37 +0200

    added custom nano syntax highlighting

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 cp bashrc ~/.bashrc
 cp nanorc ~/.nanorc
+mkdir ~/.config
+mkdir ~/.config/neofetch
 cp config.conf.neofetch ~/.config/neofetch/config.conf
+mkdir ~/.config/nano
+cp clojure.nanorc ~/.config/nano/clojure.nanorc
+cp mermaid.nanorc ~/.config/nano/mermaid.nanorc
+echo "Done"
